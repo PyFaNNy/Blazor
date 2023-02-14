22692b38286ce914a4f09faaab28c26b2aaeb039
PyFaNNy <fanny.koob@gmail.com>
2023-02-14 19:24:27 +0300

    New Initial

--- BlazorAppDemo/BlazorAppDemo/Client/Program.cs
+++ BlazorAppDemo/BlazorAppDemo/Client/Program.cs
@@ -1,11 +1,11 @@
+using BlazorAppDemo.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorAppDemo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
